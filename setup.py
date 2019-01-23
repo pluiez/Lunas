bf4886d12983c973ccf786cc8f64d40f21959a9c
pluiez <angel123goo@gmail.com>
2019-01-23 11:52:32 +0800

    remove tmp files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as r:
 
 setup(
     name='Lunas',
-    version='0.2.1',
+    version='0.2.2',
     author='Seann Zhang',
     author_email='pluiefox@live.com',
     packages=find_packages(),
